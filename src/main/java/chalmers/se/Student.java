2a1a345846f78f5503b7ec80b136767c42410ad4
Teklie <teklie@student.chalmers.se>
2019-01-10 21:36:19 +0100

    Student class outline created

---
+++ src/main/java/chalmers/se/Student.java
@@ -0,0 +1,17 @@
+package chalmers.se;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private String gradeYear;
+	private int studentId;
+	private String courses;
+	private int tuitionFee;
+	private int courseCost=400;
+	//constructor: prompt user to input student's name and year
+	//Generate id
+	// Enroll in courses
+	//View balance
+	// Pay tuition
+	//Show status
+}
