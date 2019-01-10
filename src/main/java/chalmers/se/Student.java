acd7c93703ae715383f173bd8792315857f14fd8
Teklie <teklie@student.chalmers.se>
2019-01-11 00:32:44 +0100

    Unique student id generating code added

--- src/main/java/chalmers/se/Student.java
+++ src/main/java/chalmers/se/Student.java
@@ -6,10 +6,11 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private int classYear;
-	private int studentId;
 	private String courses;
 	private int tuitionFee;
-	private int courseCost=400;
+	private static int courseCost=400;
+	private  String studentId;
+	private static int id=1000;
 	//constructor: prompt user to input student's name and year
 	public Student() {
 		Scanner in= new Scanner(System.in);
@@ -19,9 +20,17 @@ public class Student {
 		this.lastName = in.nextLine();
 		System.out.println("Enter student's class year:\n1.First year \n2. second year \n3. third year");
 		this.classYear = in.nextInt();
-		System.out.println(firstName+" "+lastName+" "+classYear);
+		this.studentId=generateStudentId();
+		System.out.println(firstName+" "+lastName+" "+classYear+" "+studentId);
+		
 	}
 	//Generate id
+	private String generateStudentId() {
+		id++;
+		//class year + static id
+		return classYear +""+id;
+		
+	}
 	// Enroll in courses
 	//View balance
 	// Pay tuition
